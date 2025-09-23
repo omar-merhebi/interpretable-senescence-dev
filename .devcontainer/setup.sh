9b3f02a7ed2c2da986f44e5e213d62566d72035c
omar-merhebi <116896797+omar-merhebi@users.noreply.github.com>
2025-09-23 21:12:00 +0000

    fix: enhance setup script logging for git configuration and streamline output

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -6,17 +6,23 @@ EXT_FILE="${EXTENSIONS_FILE:-"/workspace/.devcontainer/extensions.txt"}"
 
 log() { printf '[setup] %s\n' "$*"; }
 
-
 if [[ -n "${GIT_NAME:-}" ]]; then
     git config --global user.name "$GIT_NAME"
+    log "Git username set as $GIT_NAME"
+else
+    log "Skipped setting git username..."
 fi
 
 if [[ -n "${GIT_EMAIL:-}" ]]; then
     git config --global user.email "$GIT_EMAIL"
+    log "Git Email set as $GIT_EMAIL"
+else
+    log "Skipped setting git email..."
 fi
 
-log "Working dir: $(pwd)"
-
+log "Upgrading pip and installing Python dependencies..."
 python3 -m pip install --upgrade pip 
 cat /workspace/requirements.txt | xargs -n 1 pip install
+log "Creating symlink to data directory..."
+ln -s /data /workspace/
 log "Setup complete"
